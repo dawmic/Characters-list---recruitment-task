9265397d8f6dae914c54598c06fd589c3a79ace2
Dawid <dawmic19@gmail.com>
2022-06-21 21:59:07 +0200

    display the character list

--- src/views/CharactersList.vue
+++ src/views/CharactersList.vue
@@ -1,9 +1,19 @@
 <script setup>
 import Character from "@/components/Character.vue";
+
+const props = defineProps(["charactersList"]);
 </script>
 
 <template>
-  <main>
-    <Character />
+  <main class="main">
+    <Character v-for="item in props.charactersList" :item="item" />
   </main>
 </template>
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
