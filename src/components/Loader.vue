003299c43514f7fe15885efc4c4ca408a41f180b
Dawid <dawmic19@gmail.com>
2022-08-01 13:41:26 +0200

    Created loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="loader"></div>
+</template>
+<style lang="scss" scoped>
+.loader {
+  margin: auto;
+  width: 120px;
+  height: 20px;
+  border-radius: 20px;
+  background: radial-gradient(farthest-side, orange 94%, #0000) left/20px 20px
+    no-repeat lightblue;
+  animation: w2 1s infinite linear;
+}
+@keyframes w2 {
+  50% {
+    background-position: right;
+  }
+}
+</style>
