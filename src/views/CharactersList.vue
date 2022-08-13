80934f5a83c4a5a13a6850bf16ed7fb0dcd066ba
Dawid <dawmic19@gmail.com>
2022-08-13 16:46:04 +0200

    Connected to API and display data

--- src/views/CharactersList.vue
+++ src/views/CharactersList.vue
@@ -1,22 +1,80 @@
 <script setup>
 import Character from "@/components/Character.vue";
+import { onMounted, ref, watch } from "vue";
+import axios from "axios";
+import Loader from "../components/Loader.vue";
+import router from "../router";
+import Paginate from "../components/Paginate.vue";
+const charactersList = ref("");
+const listInfo = ref("");
+const loading = ref(true);
+const props = defineProps(["query"]);
+function getCharactersList(query) {
+  axios
+    .get(`https://rickandmortyapi.com/api/character/?${query}`)
+    .then((res) => {
+      charactersList.value = res.data.results;
+      listInfo.value = res.data.info;
+      loading.value = false;
+      window.scrollTo(0, 0);
+    })
+    .catch((err) => {
+      router.push({
+        name: "error",
+      });
+    });
+}
+onMounted(() => {
+  setTimeout(() => {
+    getCharactersList();
+  }, 2000);
+});
 
-const props = defineProps(["charactersList"]);
+watch(props, (val) => {
+  console.log("watcher dziala");
+  console.log(val.query);
+
+  getCharactersList(val.query);
+});
+function changePage(page) {
+  getCharactersList(page);
+}
 </script>
 
 <template>
   <main class="main">
-    <Character v-for="item in props.charactersList" :item="item" />
+    <Character v-for="item in charactersList" :item="item" />
+    <Loader v-if="loading" />
+    <Paginate :infoList="listInfo" @changePage="changePage" />
   </main>
 </template>
 <style lang="scss">
 .main {
-
- display: flex;
+  display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: center; 
-  
-
+  justify-content: center;
+  margin-top: 2rem;
+  position: relative;
+  min-height: calc(100vh - 8rem);
+  @include media-lg {
+    margin-left: 2rem;
+    width: 100%;
+  }
+}
+.error-container {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  z-index: 99;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .error-container__paragraph {
+    font-family: "Roboto Condensed", sans-serif;
+    font-size: 4rem;
+    color: #a50202;
+  }
 }
 </style>
