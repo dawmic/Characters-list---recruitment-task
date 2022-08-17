1e6ff8394d8eb12e2633239c954118d1acdb8b46
Dawid <dawmic19@gmail.com>
2022-08-17 22:00:30 +0200

    Deleted repeated code

--- src/App.vue
+++ src/App.vue
@@ -10,45 +10,24 @@ const list = ref("");
 const listInfo = ref("");
 const queryString = ref("");
 const showFilterPanel = ref(false);
-function getCharactersList(query) {
-  axios
-    .get(`https://rickandmortyapi.com/api/character/?${query}`)
-    .then((res) => {
-      list.value = res.data.results;
-      listInfo.value = res.data.info;
-    })
-    .catch((err) => console.log(`Something wrong ${err}`));
-}
-
-onMounted(() => {
-  getCharactersList();
-});
-const charactersList = computed(() => list.value);
-
-function getFilteredCharactersList(query) {
-  getCharactersList(query.value);
-  window.scrollTo(0, 0);
-  router.push({
-    name: "FilteredCharactersList",
-    params: { query: query.value },
-  });
-}
 
 function toggleFilterView() {
   showFilterPanel.value = !showFilterPanel.value;
-  console.log(showFilterPanel.value);
+}
+function getQuery(q) {
+  queryString.value = q;
 }
 </script>
 
 <template>
   <Header @toggleView="toggleFilterView" />
   <FiltersPanel
-    @filterCharacters="getFilteredCharactersList($event)"
     :showFilterPanel="showFilterPanel"
     @toggleView="toggleFilterView"
+    @emitQuery="getQuery"
   />
   <Transition>
-    <RouterView :charactersList="charactersList" />
+    <RouterView :query="queryString" />
   </Transition>
 </template>
 
@@ -63,13 +42,12 @@ html {
 #app {
   max-width: 190rem;
   margin: 0 auto;
-  padding: 2rem;
   font-weight: normal;
   color: $text-color;
   display: flex;
   flex-direction: column;
   margin-top: 6rem;
-  @include media-md {
+  @include media-lg {
     flex-direction: row;
   }
 }
